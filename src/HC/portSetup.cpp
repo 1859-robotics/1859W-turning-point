eda7bf59009a7adb53019e051f17326391a7bda2
alexa griffin <alexa.griffin42@gmail.com>
2018-11-24 14:30:29 -0500

    hydrocarbons

---
+++ src/HC/portSetup.cpp
@@ -0,0 +1,5 @@
+#include "portSetup.hpp"
+
+pros::Motor leftDrive = pros::Motor (LEFT_DRIVE_PORT);
+pros::Motor rightDrive = pros::Motor (RIGHT_DRIVE_PORT);
+pros::Controller master = pros::Controller (CONTROLLER_MASTER);
---
+++ src/HC/portSetup.hpp
@@ -0,0 +1,11 @@
+#include "main.h"
+
+// port defines
+#define LEFT_DRIVE_PORT 1
+#define RIGHT_DRIVE_PORT 10
+
+// components
+extern pros::Controller master;
+
+extern pros::Motor leftDrive;
+extern pros::Motor rightDrive;
