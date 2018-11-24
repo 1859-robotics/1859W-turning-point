366014bf843826f30494d87791abcc99b2189a54
alexa griffin <alexa.griffin42@gmail.com>
2018-11-24 15:59:02 -0500

    heck...

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -1,10 +1,9 @@
 #include "main.h"
 #include "app.hpp"
 
-Tracker tracker(&lTrackerWheel, &rTrackerWheel, &cTrackerWheel, &gyro);
+hc::benzene::Tracker tracker(&lTrackerWheel, &rTrackerWheel, &cTrackerWheel, &gyro);
 
 void initialize() {
-
 	tracker.debug();
 }
 
