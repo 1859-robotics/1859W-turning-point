d1792d3f92830a23b1fac858bc84c42abc289005
Griffin <alexander.griffin@stu.jessamine.kyschools.us>
2019-01-09 17:17:14 -0500

    headache

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -8,6 +8,8 @@ void autonomous() {
   robot.reset();
   posTracker.setPos({ 60, 9 }, 0);
   pros::Task track(hc::benzene::track, &posTracker);
-  
-  #include "./auton/first.auton"
+
+  robot.moveTo({ 60, 24 }, 0);
+
+  // #include "./auton/first.auton"
 }
