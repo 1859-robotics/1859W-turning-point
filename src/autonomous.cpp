fa1762b344b218a685d8ef2056bd76f4e281cade
joshfred <joshtaylorbanta@gmail.com>
2019-01-11 20:38:53 -0500

    i want to die also if you couldn't tell i have been the person making these for the past like forever

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -8,12 +8,6 @@ void autonomous() {
   robot.reset();
   pros::Task track(hc::benzene::track, &posTracker);
 
-  #include "auton/red-near.auton"
+#include "auton/red-near.auton"
 
-  //posTracker.setPos({ 0, 0 }, 0);
-
-  //FLYWHEEL_SET(FLYWHEEL_IDLE);
-
-  // robot.moveFor(12);
-  //robot.turnToFace(100);
 }
