5e87fe3338082b6b7eba1e736f30ca72bb16af2c
joshfred <joshtaylorbanta@gmail.com>
2019-01-12 09:12:16 -0500

    Revert "uhhhhhhhhhhhhh"
    This reverts commit 9a56491eb4ad63be469982d5acb8ad8cb98d079f.

--- src/HC/benzene.cpp
+++ src/HC/benzene.cpp
@@ -58,14 +58,14 @@ namespace hc {
     std::cout << "seting pos" << std::endl;
     this->x = x;
     this->y = y;
-    this->a = a;
+    this->a = TORAD(a);
   }
 
   void benzene::Tracker::setPos(benzene::Point pt, float a) {
     std::cout << "seting pos" << std::endl;
     this->x = pt.x;
     this->y = pt.y;
-    this->a = a;
+    this->a = TORAD(a);
   }
 
   void benzene::Tracker::reset() {
--- src/HC/methane.hpp
+++ src/HC/methane.hpp
@@ -9,7 +9,7 @@
 
 // robot specification macros
 #define ZERO_SPEED 30
-#define A_ERR 4
+#define A_ERR 2
 #define P_ERR 1
 
 bool withinRange(float target, float current, float error);
@@ -31,9 +31,11 @@ namespace hc {
       void seek(float x, float y, ::hc::propene::PID *transPID, ::hc::propene::PID *rotPID);
       void reset();
       void feedBall();
+      void flyUp(int rpm, std::function <void(float)> action);
       // auton functions
       void moveTo(::hc::benzene::Point target, float targetA); // moves to a point
       void moveFor(float dist);
+      void combineSet(bool rev);
         // moveTo({3, 4}, 45) - move to point (3, 4) and face 45deg
       // void moveFor(float length); // moves for a distance
         // moveFor(4) - move 4 in forwards
