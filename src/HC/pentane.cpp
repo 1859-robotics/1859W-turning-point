3b9e5f7a680879de0a0548bb6d5b33b19aef67c0
Griffin <alexander.griffin@stu.jessamine.kyschools.us>
2019-01-15 16:14:34 -0500

    hecking dang

--- src/HC/pentane.cpp
+++ src/HC/pentane.cpp
@@ -29,57 +29,81 @@ namespace hc {
     redTile.body.main_color = LV_COLOR_RED;
     redTile.body.grad_color = LV_COLOR_RED;
 
+    static lv_style_t redTilePR;
+    lv_style_copy(&redTilePR, &lv_style_plain_color);
+    redTilePR.body.main_color = LV_COLOR_RED;
+    redTilePR.body.grad_color = LV_COLOR_RED;
+    redTilePR.body.border.color = LV_COLOR_WHITE;
+    redTilePR.body.border.width = 3;
+
     static lv_style_t blueTile;
     lv_style_copy(&blueTile, &lv_style_plain_color);
     blueTile.body.main_color = LV_COLOR_BLUE;
     blueTile.body.grad_color = LV_COLOR_BLUE;
 
+    static lv_style_t blueTilePR;
+    lv_style_copy(&blueTilePR, &lv_style_plain_color);
+    blueTilePR.body.main_color = LV_COLOR_BLUE;
+    blueTilePR.body.grad_color = LV_COLOR_BLUE;
+    blueTilePR.body.border.color = LV_COLOR_WHITE;
+    blueTilePR.body.border.width = 3;
+
     // apply styles
     lv_obj_t * redA = lv_btn_create(lv_scr_act(), NULL);
     lv_obj_set_size(redA, 40, 40);
     lv_obj_set_pos(redA, 0, 80);
-    lv_obj_set_style(redA, &redTile);
+    lv_btn_set_style(redA, LV_BTN_STYLE_REL, &redTile);
+    lv_btn_set_style(redA, LV_BTN_STYLE_PR, &redTilePR);
+    lv_obj_set_free_num(redA, 1);
 
     lv_obj_t * redB = lv_btn_create(lv_scr_act(), NULL);
     lv_obj_set_size(redB, 40, 40);
     lv_obj_set_pos(redB, 0, 160);
-    lv_obj_set_style(redB, &redTile);
+    lv_btn_set_style(redB, LV_BTN_STYLE_REL, &redTile);
+    lv_btn_set_style(redB, LV_BTN_STYLE_PR, &redTilePR);
+    lv_obj_set_free_num(redB, 2);
 
     lv_obj_t * blueA = lv_btn_create(lv_scr_act(), NULL);
     lv_obj_set_size(blueA, 40, 40);
     lv_obj_set_pos(blueA, 200, 80);
-    lv_obj_set_style(blueA, &blueTile);
+    lv_btn_set_style(blueA, LV_BTN_STYLE_REL, &blueTile);
+    lv_btn_set_style(blueA, LV_BTN_STYLE_PR, &blueTilePR);
+    lv_obj_set_free_num(blueA, 3);
 
     lv_obj_t * blueB = lv_btn_create(lv_scr_act(), NULL);
     lv_obj_set_size(blueB, 40, 40);
     lv_obj_set_pos(blueB, 200, 160);
-    lv_obj_set_style(blueB, &blueTile);
+    lv_btn_set_style(blueB, LV_BTN_STYLE_REL, &blueTile);
+    lv_btn_set_style(blueB, LV_BTN_STYLE_PR, &blueTilePR);
+    lv_obj_set_free_num(blueB, 4);
     // change active buttons
 
     // bind functions to output
     lv_btn_set_action(blueA, LV_BTN_ACTION_PR, [](lv_obj_t * btn) -> lv_res_t {
       hc::pentane::selectedTile = 1;
-      lv_obj_get_free_num(btn);
-      std::cout << "hello from 1" << std::endl;
+      // lv_btn_set_state(btn, LV_BTN_STATE_TGL_REL);
+      std::cout << "hello from: " << hc::pentane::selectedTile << std::endl;
       return LV_RES_OK;
     });
     lv_btn_set_action(blueB, LV_BTN_ACTION_PR, [](lv_obj_t * btn) -> lv_res_t {
       hc::pentane::selectedTile = 2;
-      lv_obj_get_free_num(btn);
-      std::cout << "hello from 2" << std::endl;
+      // lv_btn_set_state(btn, LV_BTN_STATE_TGL_REL);
+      std::cout << "hello from: " << hc::pentane::selectedTile << std::endl;
       return LV_RES_OK;
     });
     lv_btn_set_action(redA, LV_BTN_ACTION_PR, [](lv_obj_t * btn) -> lv_res_t {
       hc::pentane::selectedTile = 3;
-      lv_obj_get_free_num(btn);
-      std::cout << "hello from 3" << std::endl;
+      // lv_btn_set_state(btn, LV_BTN_STATE_TGL_REL);
+      std::cout << "hello from: " << hc::pentane::selectedTile << std::endl;
       return LV_RES_OK;
+      lv_btn_set_style(btn, LV_BTN_STYLE_REL, &blueTile);
     });
     lv_btn_set_action(redB, LV_BTN_ACTION_PR, [](lv_obj_t * btn) -> lv_res_t {
       hc::pentane::selectedTile = 4;
-      lv_obj_get_free_num(btn);
-      std::cout << "hello from 4" << std::endl;
+      // lv_btn_set_state(btn, LV_BTN_STATE_REL);
+      std::cout << "hello from: " << hc::pentane::selectedTile << std::endl;
       return LV_RES_OK;
+      lv_btn_set_style(btn, LV_BTN_STYLE_REL, &blueTile);
     });
   }
 }
--- src/initialize.cpp
+++ src/initialize.cpp
@@ -12,10 +12,11 @@ void initialize() {
   init();
   hc::pentane::drawFeild();
   std::cout << "hello from init" << std::endl;
-  
+
   lv_obj_t * hecker = lv_label_create(lv_scr_act(), NULL);
   lv_obj_align(hecker, NULL, LV_ALIGN_CENTER, 0, 20);
   hc::pentane::drawTiles([=](int output) {
+    std::cout << "hello" << std::endl;
     lv_label_set_text(hecker, (std::to_string(output)).c_str());
     hc::pentane::selectedTile = output;
   });
