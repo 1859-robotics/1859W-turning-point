35e6c419594189602b40e9a5877a9bffd2289e48
alexa griffin <alexa.griffin42@gmail.com>
2019-01-16 09:45:42 -0500

    why am i doing this

---
+++ auton-builder/config.js
@@ -0,0 +1,31 @@
+export default {
+  autonDir: "../src/auton",
+  outputFile: "../src/autonomous.cpp",
+  tiles: {
+    redA: [
+      {
+        name: "testing redA",
+        auton: "../src/auton/blue-far-1.auton"
+      }
+    ],
+    redB: [
+      {
+        name: "testing redB",
+        auton: "../src/auton/blue-far-1.auton"
+      }
+    ],
+    blueA: [
+      {
+        name: "testing blueA",
+        auton: "../src/auton/blue-far-1.auton"
+      }
+    ],
+    blueB: [
+      {
+        name: "testing blueB",
+        auton: "../src/auton/blue-far-1.auton"
+      }
+    ]
+
+  }
+}
