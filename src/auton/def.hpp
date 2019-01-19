40212e1ff154bdec32da55b50e7d5e51162181ec
Griffin <alexander.griffin@stu.jessamine.kyschools.us>
2019-01-18 20:38:58 -0500

    i hate my life so much

--- src/auton/def.hpp
+++ src/auton/def.hpp
@@ -9,14 +9,16 @@
 #define AUTON_NAME_BLUE_A "BLUE A"
 #define AUTON_NAME_BLUE_B "BLUE B"
 
-#define TILE_RED_A 3
-#define TILE_RED_B 4
 #define TILE_BLUE_A 1
 #define TILE_BLUE_B 2
+#define TILE_RED_A 3
+#define TILE_RED_B 4
 
 // auton names
-#define BLUE_NEAR_1  "alliance post"
-#define BLUE_FAR_1   "center post"
+#define BLUE_NEAR_1   "alliance post & cap"
+#define BLUE_NEAR_2   "alliance post"
+#define BLUE_FAR_1    "center post & park"
+#define BLUE_FAR_2    "center post & cap"
 
 #define RED_NEAR_1   "alliance post & cap"
 #define RED_NEAR_2   "alliance post"
@@ -27,12 +29,17 @@
 #define AUTON_OPTIONS_RED_A "none" "\n" \
                             RED_NEAR_1 "\n" \
                             RED_NEAR_2
+
 #define AUTON_OPTIONS_RED_B "none" "\n" \
                             RED_FAR_1 "\n" \
                             RED_FAR_2
+
 #define AUTON_OPTIONS_BLUE_A "none" "\n" \
-                             BLUE_NEAR_1
+                             BLUE_NEAR_1 "\n" \
+                             BLUE_NEAR_2
+                             
 #define AUTON_OPTIONS_BLUE_B "none" "\n" \
-                             BLUE_FAR_1
+                             BLUE_FAR_1 "\n" \
+                             BLUE_FAR_2
 
 #endif
--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -12,6 +12,7 @@ void runAuton() {
     }
     return;
   }
+
   if(hc::pentane::selectedTile == TILE_RED_A) {
     std::cout << "runing on tile: " << TILE_RED_A << std::endl;
     if(hc::pentane::selectedAuton == RED_NEAR_1) {
@@ -28,21 +29,25 @@ void runAuton() {
     }
   } else if(hc::pentane::selectedTile == TILE_BLUE_A) {
     std::cout << "runing on tile: " << TILE_BLUE_A << std::endl;
-    if(hc::pentane::selectedAuton == BLUE_NEAR_1) {
-      #include "./auton/blue-near-1.auton"
+    if(hc::pentane::selectedAuton == BLUE_FAR_1) {
+      #include "./auton/blue-far-1.auton"
+    } else if(hc::pentane::selectedAuton == BLUE_FAR_2) {
+      #include "./auton/blue-far-0.auton"
     }
   } else if(hc::pentane::selectedTile == TILE_BLUE_B) {
     std::cout << "runing on tile: " << TILE_BLUE_B << std::endl;
-    if(hc::pentane::selectedAuton == BLUE_FAR_1) {
-      #include "./auton/blue-far-1.auton"
+    if(hc::pentane::selectedAuton == BLUE_NEAR_1) {
+      #include "./auton/blue-near-1.auton"
+    } else if(hc::pentane::selectedAuton == BLUE_NEAR_2) {
+      #include "./auton/blue-near-0.auton"
     }
   }
 }
 
 void autonomous() {
   robot.reset();
-      pros::Task track(hc::benzene::track, &posTracker);
+  pros::Task track(hc::benzene::track, &posTracker);
 
-  runAuton();
+    runAuton();
   track.remove();
 }
