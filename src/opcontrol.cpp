a734fb7537682170ab45266116c0e1c923412027
joshfred <joshtaylorbanta@gmail.com>
2019-02-02 14:53:11 -0500

    comp changes

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -10,23 +10,24 @@ void opcontrol() {
 
     bool autonOverwrite = false;
 
-		if(master.get_digital(DIGITAL_UP)) {
-			rightDriveF.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-			rightDriveB.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-			leftDriveF.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-			leftDriveB.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-		} else {
-			rightDriveF.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-			rightDriveB.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-			leftDriveF.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-			leftDriveB.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-		}
+		//if(master.get_digital(DIGITAL_UP)) {
+		//	rightDriveF.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+		//	rightDriveB.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+		//	leftDriveF.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+		//	leftDriveB.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+		//} else {
+		//	rightDriveF.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+		//	rightDriveB.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+		//	leftDriveF.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+		//	leftDriveB.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+		//}
 
-		if(master.get_digital(DIGITAL_DOWN)) {
+		if(master.get_digital(DIGITAL_R2)) {
 			COMBINE_SET(0);
 		} else {
 			robot.combineSet(master.get_digital(DIGITAL_L1));
-		}
+
+	}
 
     if(master.get_digital(DIGITAL_Y)) {
       FLYWHEEL_SET(127);
@@ -43,7 +44,7 @@ void opcontrol() {
     if(!autonOverwrite) {
       if(master.get_digital(DIGITAL_R1)) {
         INTAKE_SET(127);
-      } else if(master.get_digital(DIGITAL_R2)) {
+      } else if(master.get_digital(DIGITAL_UP)) {
         INTAKE_SET(-127);
       } else {
         INTAKE_SET(0);
