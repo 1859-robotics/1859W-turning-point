8e7bb7263e73d12436fc3207ac520dc542b04a73
alexa griffin <alexa.griffin42@gmail.com>
2019-02-03 21:20:28 -0500

    heckl

--- auton-builder/config.js
+++ auton-builder/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  path: "../src/auton/",
+  path: "./src/auton/",
   validTiles: [ "BLUE_A", "BLUE_B", "RED_A", "RED_B" ],
-  outputDef: "../src/auton/def.hpp",
-  outputAuton: "../src/runAuton"
+  outputDef: "./src/auton/def.hpp",
+  outputAuton: "./src/runAuton"
 }
