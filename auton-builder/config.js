56b38895d78e36db471c657b8ba32597d4a9cdeb
alexa griffin <alexa.griffin42@gmail.com>
2019-02-03 20:25:20 -0500

    derp

--- auton-builder/config.js
+++ auton-builder/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  path: "../src/auton"
+  path: "../src/auton/",
+  validTiles: [ "TILE_BLUE_A", "TILE_BLUE_B", "TILE_RED_A", "TILE_RED_B" ]
 }
--- auton-builder/index.js
+++ auton-builder/index.js
@@ -4,4 +4,24 @@ const config = require("./config.js")
 // get .auton files
 const autons = fs.readdirSync(config.path).filter(file => file.indexOf(".auton") !== -1)
 
+let autonList = []
+
+autons.forEach(file => {
+  const auton = fs.readFileSync(config.path + file, "utf8")
+  const meta = auton.match(/#meta .*/g) // get meta directives
+  let script = auton.replace(/#meta .*/g, "")
+
+  let metaData = {}
+
+  if(!meta) {
+    console.log("auton file must contain meta name | " + file)
+  } else {
+    meta.forEach(m => { // extract meta data
+      metaData[m.split(" ")[1]] = m.split(" ")[2]
+    })
+    console.log(metaData)
+
+  }
+})
+
 console.log(autons)
