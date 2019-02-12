faec2052ab32831d2e9fd3a21d85b87d4f592786
Griffin <alexander.griffin@stu.jessamine.kyschools.us>
2019-02-12 17:26:16 -0500

    everything hurts

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -6,10 +6,8 @@ void opcontrol() {
   pros::Task posTracker(odom::trackTask, tracker);
 
   while(true) {
-
     RIGHT_DRIVE_SET(master.get_analog(ANALOG_RIGHT_Y));
     LEFT_DRIVE_SET(master.get_analog(ANALOG_LEFT_Y));
-
 
     pros::delay(20);
   }
