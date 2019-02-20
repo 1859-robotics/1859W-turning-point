5e4267136321f5db36ca746392d528fd7731be60
Griffin <alexander.griffin@stu.jessamine.kyschools.us>
2019-02-20 12:52:31 -0500

    update jctc api

---
+++ include/jctc/chassis/chassis.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include "main.h"
+#include "../misc/misc.hpp"
+#include "../odom/odom.hpp"
+#include "../odom/position.hpp"
+#include "../pid/pid.hpp"
+
+#ifndef P_ERR
+  #define P_ERR 3
+#endif
+
+#ifndef A_ERR
+  #define A_ERR 3
+#endif
+
+namespace jctc {
+  class Chassis {
+  private:
+    odom::Tracker tracker;
+
+    PID turnPid;
+    PID distPid;
+
+  public:
+    Chassis(odom::Tracker tracker, PID turnPid, PID distPid) :
+      tracker(tracker), turnPid(turnPid), distPid(distPid) {};
+
+    virtual void driveVector(float forward, float turn) ;
+
+    void moveTo(odom::Point target, float rotScalar, int timeout, float err);
+    void moveToSimple(odom::Point target, int timeout = 5000);
+
+    void moveFor(float distIn, float exit = 5000);
+    void moveFor(float distIn, PIDConfig pid, float exit = 5000);
+
+    void turnToFace(float target, int timeout = 5000, float err = P_ERR);
+    void turnToFace(odom::Point target, int timeout = 5000, float err = P_ERR);
+
+    float angleToPoint(odom::Point target);
+    float distanceToPoint(odom::Point target);
+  };
+}
