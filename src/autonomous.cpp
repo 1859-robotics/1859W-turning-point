8b443c8215194a7b7eec36ff2ac5d7e71a2aa202
Griffin <alexander.griffin@stu.jessamine.kyschools.us>
2019-02-20 09:06:07 -0500

    heck

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,3 +1,11 @@
 #include "main.h"
+#include "robot-config.hpp"
 
-void autonomous() {}
+void autonomous() {
+  pros::Task posTracker(odom::trackTask, tracker);
+
+  tracker->reset();
+  tracker->setPos({ 0, 0, PI / 2 });
+
+  robot.driveTo({ 9, 9 }, 2.5, 0, 5);
+}
