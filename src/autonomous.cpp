78a05205b915aaebba0cbe5439df575d444992a1
Griffin <alexander.griffin@stu.jessamine.kyschools.us>
2019-03-05 18:22:44 -0500

    everything hurts

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -4,13 +4,16 @@
 void autonomous() {
   robot.reset();
   tracker->reset();
-  tracker->setPos(0, 0, TORAD(90));
+  tracker->setPos(0, 0, TORAD(0));
   pros::delay(500);
   pros::Task posTracker(odom::trackTask, tracker);
 
-  // robot.turnToFace(90);
+  // robot.turnToFace({ 10, 10 });
+  // std::cout << "after" << std::endl;
 
-  robot.moveFor(20);
+  robot.moveToSimple({ 10, 10 });
+  pros::delay(2000);
+  robot.moveToSimple({ 10, 10 });
 
   while(true) {
     pros::delay(20);
--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -2,7 +2,6 @@
 #include "robot-config.hpp"
 #include <iostream>
 
-
 static float round(float var) {
     float value = (int)(var * 100 + .5);
     return (float)value / 100;
@@ -12,56 +11,70 @@ static bool withinRange(float target, float current, float error) {
   return fabs(target - current) < error;
 }
 
+
 void opcontrol() {
-  pros::Task posTracker(odom::trackTask, tracker);
-  Logger posLogger(LV_ALIGN_CENTER, "tracker");
-  tracker->reset();
-  tracker->setPos(0, 0, TORAD(90));
-
-  TASK_WHILE(true, {
-    posLogger.log(("(" + std::to_string(round(tracker->getPos().pos.x)) + ","
-                       + std::to_string(round(tracker->getPos().pos.y)) + ")  | " +
-                         std::to_string(round(TODEG(tracker->getPos().a)))));
-
+	while (true) {
     float rV = master.get_analog(ANALOG_RIGHT_Y);
     float lV = master.get_analog(ANALOG_LEFT_Y);
 
- 		rV = abs(rV) > 15 ? rV : 0;
- 		lV = abs(lV) > 15 ? lV : 0;
+		rV = abs(rV) > 15 ? rV : 0;
+		lV = abs(lV) > 15 ? lV : 0;
 
- 		if(rV == 0 && lV == 0) {
- 			// negative to reverse controlls
- 			rV = -partner.get_analog(ANALOG_LEFT_Y);
- 			lV = -partner.get_analog(ANALOG_RIGHT_Y);
- 		}
+		if(rV == 0 && lV == 0) {
+			// reversed to reverse controlls
+			rV = -partner.get_analog(ANALOG_LEFT_Y);
+			lV = -partner.get_analog(ANALOG_RIGHT_Y);
+		}
 
- 		RIGHT_DRIVE_SET(abs(rV) > 15 ? rV : 0);
- 		LEFT_DRIVE_SET(abs(lV) > 15 ? lV : 0);
+		RIGHT_DRIVE_SET(abs(rV) > 15 ? rV : 0);
+		LEFT_DRIVE_SET(abs(lV) > 15 ? lV : 0);
 
     bool autonOverwrite = false;
 
-    if(master.get_digital(DIGITAL_R2)) {
-			COMBINE_SET(0);
+		if(partner.get_digital(DIGITAL_L2)) {
+			rightDriveF.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+			rightDriveB.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+			leftDriveF.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+			leftDriveB.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+			RIGHT_DRIVE_SET(0);
+			LEFT_DRIVE_SET(0);
+	 	} else {
+		 	rightDriveF.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+		 	rightDriveB.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+		 	leftDriveF.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+		 	leftDriveB.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+	 	}
+
+		if(limit.get_value()) {
+			INTAKE_SET(0);
+		}
+
+		if(master.get_digital(DIGITAL_R1)) {
+			INTAKE_SET(-127);
+		} else if (limit.get_value() || master.get_digital(DIGITAL_R2)) {
+			INTAKE_SET(0);
 		} else {
-			robot.combineSet(master.get_digital(DIGITAL_L2));
-    }
+			INTAKE_SET(127);
+		}
 
-    if(partner.get_digital(DIGITAL_LEFT) || master.get_digital(DIGITAL_LEFT)) {
-			flipover.move(-127);
-		} else if(partner.get_digital(DIGITAL_RIGHT) || master.get_digital(DIGITAL_RIGHT)) {
-			flipover.move(127);
-    } else flipover.move(0);
+		std::cout << flipover.get_position() << std::endl;
 
-    LOG_VAR(lTrackerWheel.get_value());
-    LOG_VAR(rTrackerWheel.get_value());
-    LOG_VAR(cTrackerWheel.get_value());
+		if(partner.get_digital(DIGITAL_A)) {
+			flipover.move_absolute(2100, 600);
+		} else if(partner.get_digital(DIGITAL_B)) {
+			flipover.move_absolute(3900, 600);
+		} else if(partner.get_digital(DIGITAL_Y)) {
+			flipover.move_absolute(0, 600);
+		} else if(partner.get_digital(DIGITAL_X)) {
+			flipover.move_absolute(1090, 600);
+		}
 
     if(master.get_digital(DIGITAL_Y)) {
       FLYWHEEL_SET(127);
     } else if(master.get_digital(DIGITAL_B)) {
       OPCONTROL_FLYWHEEL_SET(480);
-		} else if(master.get_digital(DIGITAL_A)) {
-	    OPCONTROL_FLYWHEEL_SET(370);
+		} else if(master.get_digital(DIGITAL_L2)) {
+	    OPCONTROL_FLYWHEEL_SET(440);
     } else if(master.get_digital(DIGITAL_L1)) {
       OPCONTROL_FLYWHEEL_SET(520);
     } else if(master.get_digital(DIGITAL_X)) {
@@ -70,14 +83,16 @@ void opcontrol() {
       FLYWHEEL_SET(FLYWHEEL_IDLE);
     }
 
-    if(!autonOverwrite) {
-      if(master.get_digital(DIGITAL_UP)) {
-        INTAKE_SET(-127);
-      } else if(master.get_digital(DIGITAL_DOWN) || !limit.get_value()) {
-        INTAKE_SET(127);
-      } else {
-        INTAKE_SET(0);
-      }
+		if(partner.get_digital(DIGITAL_R1) || master.get_digital(DIGITAL_LEFT)) {
+			flipover.move(-127);
+		} else if(partner.get_digital(DIGITAL_R2) || master.get_digital(DIGITAL_RIGHT)) {
+			flipover.move(127);
+    } else flipover.move(0);
+
+    if(limit.get_value()) {
+      master.rumble("-");
     }
-  })
+
+		pros::delay(20);
+	}
 }
