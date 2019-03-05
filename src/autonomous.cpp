3602c0fb4d06e633ac31886a2367ccbc5887f298
Griffin <alexander.griffin@stu.jessamine.kyschools.us>
2019-03-05 09:15:20 -0500

    heck

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -4,10 +4,13 @@
 void autonomous() {
   robot.reset();
   tracker->reset();
-  tracker->setPos(0, 0, TORAD(90));
+  tracker->setPos(0, 0, TORAD(0));
   pros::Task posTracker(odom::trackTask, tracker);
 
-  robot.moveToTest({ 2, 10 }, TORAD(30));
+  robot.turnToFace(90);
+
+  // robot.moveToTest({ 2, 10 }, TORAD(30));
+
   while(true) {
     pros::delay(20);
   }
