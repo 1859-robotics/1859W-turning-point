ebef30324ddef36739c5b2cfe48ccc94016bab42
joshfred <joshtaylorbanta@gmail.com>
2019-03-08 13:49:49 -0500

    ahahaha

--- src/auton/def.hpp
+++ src/auton/def.hpp
@@ -9,7 +9,7 @@
 
 #define AUTON_OPTIONS_BLUE_A "nothing" "\n" "post&cap" "\n" "post&park" "\n" "Allpost&cap"
 #define AUTON_OPTIONS_BLUE_B "nothing" "\n" "park"
-#define AUTON_OPTIONS_RED_A "nothing" "\n" "SkillsAllpost&cap" "\n" "post&cap" "\n" "Allpost&cap" "\n" "Allpost&Park" "\n" "skills 10" "\n" "skills 20" "\n" "skills 11" "\n" "skills 14" "\n" "skills 11"
-#define AUTON_OPTIONS_RED_B "nothing" "\n" "park" "\n" "skills 14" "\n" "skills NEW14" "\n" "skills 14"
+#define AUTON_OPTIONS_RED_A "nothing" "\n" "SkillsAllpost&cap" "\n" "post&cap" "\n" "Allpost&cap" "\n" "Allpost&Park"
+#define AUTON_OPTIONS_RED_B "nothing" "\n" "park" "\n" "skills NEW14" "\n" "skills 14"
 
 #endif
